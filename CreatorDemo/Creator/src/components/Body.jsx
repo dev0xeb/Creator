ce8edb78950120009355e66dd258545832f2dfff
dev0xeb <clintonayomide96@gmail.com>
2025-01-29 20:53:54 +0100

    1-29-25

--- CreatorDemo/Creator/src/components/Body.jsx
+++ CreatorDemo/Creator/src/components/Body.jsx
@@ -1,45 +1,56 @@
-import React from 'react'
-import ArcaneKnight from '../images/ArcaneKnight.jpg'
-import ninja from '../images/cyberNinja.jpg'
-import raider from '../images/desertRaider.jpg'
-import futuristicSoldier from '../images/FurturisticSoldier.jpg'
-import jungleTracker from '../images/jungleTracker.jpg'
-import mutantMercnary from '../images/MutantMercnary.jpg'
-import scanvenger from '../images/Scanvenger.jpg'
-import shadowSamurai from '../images/shadowSmaurai.jpg'
-import spaceRanger from '../images/spaceRanger.jpg'
-import valkeylie from '../images/Valkeylie.jpg'
-import '../index.css'
+import React from 'react';
+import ArcaneKnight from '../images/ArcaneKnight.jpg';
+import ninja from '../images/cyberNinja.jpg';
+import raider from '../images/desertRaider.jpg';
+import futuristicSoldier from '../images/FurturisticSoldier.jpg';
+import jungleTracker from '../images/jungleTracker.jpg';
+import mutantMercnary from '../images/MutantMercnary.jpg';
+import scanvenger from '../images/Scanvenger.jpg';
+import shadowSamurai from '../images/shadowSmaurai.jpg';
+import spaceRanger from '../images/spaceRanger.jpg';
+import valkeylie from '../images/Valkeylie.jpg';
+import '../index.css';
 
 const Body = () => {
   return (
     <div>
       <div className='banner'>
-        <div className='slider' style={{'--quantity': 10}}>
-            <div className='item' style= {{'--position': 1}}><img src={ArcaneKnight} alt="" /></div>
-            <div className='item' style= {{'--position': 2}}><img src={ninja} alt="" /></div>
-            <div className='item' style= {{'--position': 3}}><img src={raider} alt="" /></div>
-            <div className='item' style= {{'--position': 4}}><img src={futuristicSoldier} alt="" /></div>
-            <div className='item' style= {{'--position': 5}}><img src={jungleTracker} alt="" /></div>
-            <div className='item' style= {{'--position': 6}}><img src={mutantMercnary} alt="" /></div>
-            <div className='item' style= {{'--position': 7}}><img src={scanvenger} alt="" /></div>
-            <div className='item' style= {{'--position': 8}}><img src={shadowSamurai} alt="" /></div>
-            <div className='item' style= {{'--position': 9}}><img src={spaceRanger} alt="" /></div>
-            <div className='item' style= {{'--position': 10}}><img src={valkeylie} alt="" /></div>
+        {/* 3D Slider */}
+        <div className='slider' style={{ '--quantity': 10 }}>
+          {[
+            ArcaneKnight,
+            ninja,
+            raider,
+            futuristicSoldier,
+            jungleTracker,
+            mutantMercnary,
+            scanvenger,
+            shadowSamurai,
+            spaceRanger,
+            valkeylie,
+          ].map((image, index) => (
+            <div
+              key={index}
+              className='item'
+              style={{ '--position': index + 1 }}
+            >
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
         </div>
+
+        {/* Content Section */}
         <div className='content'>
-          <h1 data-content= 'COMING SOON...'>COMING SOON...</h1>
+          <h1 data-content='COMING SOON...'>COMING SOON...</h1>
           <div className='author'>
             <h2>CREATOR</h2>
             <p>You Can be what you Want</p>
           </div>
-          <div className='model'>
-
-          </div>
+          <div className='model'></div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
